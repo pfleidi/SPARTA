cfef7eabd91997b36a3d28505ebb478e56e864bc
Dominik Huebner <contact@dhuebner.com>
2011-11-13 16:34:31 +0100

    Added connection handling to AmazonEC2 instance provider

--- ext/instance_providers/amazon_ec2.rb
+++ ext/instance_providers/amazon_ec2.rb
@@ -1,17 +1,31 @@
+require 'fog'
+
 class AmazonEC2 < Sparta::Providers
   def self.create_instance(env = {})
+    connection = self.connect(env)
 
+    instance = connection.servers.create
+    instance.wait_for { ready? }
+
+    instance.id
   end
 
   def self.destroy_instance(env = {})
-  
+    connection = self.connect(env)
+    instance = connection.servers.get(env[:id]).destroy
   end
 
-  def self.clean_up(env = {})
+  def self.connect(env = {})
+    Fog.mock!
+    name = self.name.to_s.downcase.to_sym
 
-  end
+    credentials = env[:credentials]
+    credentials ||= Sparta::Credentials.providers[name]
 
-  def self.connect(args)
-
+    connection = Fog::Compute.new({
+      :provider => 'AWS',
+      :aws_access_key_id => credentials[:id],
+      :aws_secret_access_key => credentials[:key]
+    })
   end
 end
