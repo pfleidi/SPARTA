66c989967956de1ed1f68379a5ffd6a4e685fdb0
Dominik Huebner <contact@dhuebner.com>
2011-11-13 16:12:59 +0100

    Added AmazonEC2 provider skeleton

---
+++ ext/instance_providers/amazon_ec2.rb
@@ -0,0 +1,17 @@
+class AmazonEC2 < Sparta::Providers
+  def self.create_instance(env = {})
+
+  end
+
+  def self.destroy_instance(env = {})
+  
+  end
+
+  def self.clean_up(env = {})
+
+  end
+
+  def self.connect(args)
+
+  end
+end
