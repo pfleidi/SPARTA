71f002e85c34baf4313cbadcbccb7306887f3430
Dominik Huebner <contact@dhuebner.com>
2011-11-15 22:19:05 +0100

    Fixed amazon instance bootstrapping

--- ext/instance_providers/amazon_ec2.rb
+++ ext/instance_providers/amazon_ec2.rb
@@ -4,6 +4,7 @@ require 'bundler/setup'
 require "fog"
 
 class AmazonEC2 < Sparta::Providers
+
   def self.connect(env = {})
     name = self.name.to_s.downcase.to_sym
 
@@ -13,13 +14,49 @@ class AmazonEC2 < Sparta::Providers
     connection = Fog::Compute.new({
       :provider => 'AWS',
       :aws_access_key_id => credentials[:id],
-      :aws_secret_access_key => credentials[:key]
+      :aws_secret_access_key => credentials[:key],
     })
   end
 
   def self.create_instance(env = {})
     connection = connect(env)
-    instance = connection.servers.bootstrap(env)
+    
+    if connection.key_pairs.get('sparta_tmp_kp')
+      connection.key_pairs.get('sparta_tmp_kp').destroy
+    end
+
+    key_pair = connection.key_pairs.create(:name => 'sparta_tmp_kp')
+    env[:key_pair] = key_pair
+
+    instance = connection.servers.create(env)
+    instance.wait_for { ready? }
+
+    begin
+      Timeout::timeout(360) do
+        begin
+          Timeout::timeout(8) do
+            Net::SSH.start(instance.public_ip_address, instance.username, {:key_data => key_pair.private_key}) do |ssh|
+              ssh.exec('pwd')
+            end
+          end
+        rescue Errno::ECONNREFUSED
+          sleep(2)
+          retry
+        rescue Net::SSH::AuthenticationFailed, Timeout::Error
+          retry
+        end
+      end
+
+    rescue Timeout::Error
+      instance.destroy
+    end
+    
+    instance_data = {
+      :id => instance.id,
+      :username => instance.username,
+      :private_key => key_pair.private_key,
+      :host => instance.public_ip_address
+    }
   end
 
   def self.destroy_instance(env = {})
--- lib/sparta/warrior.rb
+++ lib/sparta/warrior.rb
@@ -2,18 +2,15 @@ require 'rubygems'
 require 'bundler/setup'
 
 require 'sshkey'
+require 'net/ssh'
 
 module Sparta
   class Warrior
-    attr_reader :instance, :provider, :key_pair, :credentials
+    attr_reader :instance, :provider, :credentials
 
     def initialize(env = {})
       @provider = env[:provider]
       @credentials = env[:credentials]
-      @key_pair = SSHKey.generate
-
-      env[:private_key] = @key_pair.private_key
-      env[:public_key] = @key_pair.ssh_public_key
 
       @instance = Providers.providers[@provider].create_instance(env)
     end
@@ -33,7 +30,13 @@ module Sparta
 
     end
 
-    def ssh(commands = [])
+    def ssh(command)
+      Net::SSH.start(@instance[:host], @instance[:username], {:key_data  => @instance[:private_key]}) do |ssh|
+        ssh.exec(command)
+      end
+    end
+
+    def package_manager
     end
   end
 end
