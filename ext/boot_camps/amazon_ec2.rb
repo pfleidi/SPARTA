26529d03d7f2a3aed99e38f7444947e32709cdbe
Sven Pfleiderer <sg+github@roothausen.de>
2011-12-08 13:06:02 +0100

    Add custom key name modification to AWS bootcamp

--- ext/boot_camps/amazon_ec2.rb
+++ ext/boot_camps/amazon_ec2.rb
@@ -4,28 +4,36 @@ require 'net/netrc'
 require "fog"
 
 class AmazonEC2 < Sparta::BootCamp
-  def initialize(credentials, opts)
+
+  def initialize(credentials, opts = {})
     @credentials = credentials
     @opts = opts
   end
 
   def connect!(env = {})
-    @connection = Fog::Compute.new({
+    # SSH key pair name
+    Fog.credential = @opts[:key_pair_name] || :sparta_key_pair
+
+    @connection = Fog::Compute.new(
       :provider => 'AWS',
       :aws_access_key_id => @credentials.login,
       :aws_secret_access_key => @credentials.password,
-      })
+    )
 
-      @instance = @connection.servers.bootstrap({:private_key_path => '~/.ssh/id_rsa', :public_key_path => '~/.ssh/id_rsa.pub'})
-      Sparta::BootCamp.running_instances << @instance
-      @instance.id
-    end
+    @instance = @connection.servers.bootstrap(
+      :private_key_path => '~/.ssh/id_rsa',
+      :public_key_path  => '~/.ssh/id_rsa.pub'
+    )
+    Sparta::BootCamp.running_instances << @instance
 
-    def kill!
-      @instance.destroy
-    end
-    
-    def ssh(command = "")
-      @instance.ssh(command)
-    end
+    @instance.id
   end
+
+  def kill!
+    @instance.destroy
+  end
+
+  def ssh(command = "")
+    @instance.ssh(command)
+  end
+end
