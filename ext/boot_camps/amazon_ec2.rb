11638f3f3757c9ca9e9e7619125f00dbf91f2b56
Dominik Huebner <contact@dhuebner.com>
2011-12-08 13:22:51 +0100

    added provider for static hosts

--- ext/boot_camps/amazon_ec2.rb
+++ ext/boot_camps/amazon_ec2.rb
@@ -14,18 +14,18 @@ class AmazonEC2 < Sparta::BootCamp
       :provider => 'AWS',
       :aws_access_key_id => @credentials.login,
       :aws_secret_access_key => @credentials.password,
-      })
+    })
 
-      @instance = @connection.servers.bootstrap({:private_key_path => '~/.ssh/id_rsa', :public_key_path => '~/.ssh/id_rsa.pub'})
-      Sparta::BootCamp.running_instances << @instance
-      @instance.id
-    end
-
-    def kill!
-      @instance.destroy
-    end
-    
-    def ssh(command = "")
-      @instance.ssh(command)
-    end
+    @instance = @connection.servers.bootstrap({:private_key_path => '~/.ssh/id_rsa', :public_key_path => '~/.ssh/id_rsa.pub'})
+    Sparta::BootCamp.running_instances << @instance
+    @instance.id
   end
+
+  def kill!
+    @instance.destroy
+  end
+
+  def ssh(command = "")
+    @instance.ssh(command)
+  end
+end
