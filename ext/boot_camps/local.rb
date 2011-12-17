aefd0a64447611d91e9ab6906517e5d5a23dd203
Sven Pfleiderer <sg+github@roothausen.de>
2011-12-17 15:52:37 +0100

    boot_camps/local: ssh now returns a valid ssh response object

--- ext/boot_camps/local.rb
+++ ext/boot_camps/local.rb
@@ -1,30 +1,35 @@
+require "fog"
 
 class LocalProvider < Sparta::BootCamp
-  
+
   def initialize(credentials, options)
     #noop
   end
-  
+
   def connection
     return "no connection.."
   end
-  
+
   def connect!
-    
+
   end
-  
+
   def ssh(command)
     # some redirection here..
     temp_out = StringIO.new
     temp_err = StringIO.new
-      old_stdout, $stdout = $stdout, temp_out
-      old_stderr, $stderr = $stderr, temp_err
-      system(command)
-      $stdout = old_stdout 
-      $stderr = old_stderr
-      actual = temp_out.string
-      error = temp_err.string
-      [actual,error]
+    old_stdout, $stdout = $stdout, temp_out
+    old_stderr, $stderr = $stderr, temp_err
+    system(command)
+    $stdout = old_stdout
+    $stderr = old_stderr
+    actual = temp_out.string
+    error = temp_err.string
+
+    result = Fog::SSH::Result.new(command)
+    result.status = 0
+
+    result
   end
 
   def kill!
