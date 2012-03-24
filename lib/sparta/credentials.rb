d87bb116f7d0ee1731063325b499d4697fdb06c2
Sven Pfleiderer <sg+github@roothausen.de>
2012-03-24 15:29:29 +0100

    Clean up credentials

--- lib/sparta/credentials.rb
+++ lib/sparta/credentials.rb
@@ -5,36 +5,35 @@ module Sparta
   class Credentials
     attr_reader :login
     attr_reader :password
-    
+
     def initialize(login,password)
       @login = login
       @password = password
     end
-    
+
     def self.provide_for_provider(provider)
-      # try to load from credentials file 
-      new_credentials = nil
+      # try to load from credentials file
       credential_file = File.open('config/credentials.yaml')
-      if ( credential_file )
+      if credential_file
         contents = YAML.load(credential_file)
-        if ( contents[provider] )
+        if contents[provider]
           provider_contents = contents[provider]
           new_credentials = Credentials.new(provider_contents['id'], provider_contents['key'])
         end
       end
-      
+
       # if there are no credentials here, see for .netrc
-      
-      if ( !new_credentials )
+
+      if new_credentials.nil?
         credentials = Net::Netrc.locate("sparta.#{provider}")
-        if ( credentials )
+        if credentials
           new_credentials = Credentials.new(credentials.login, credentials.password)
         end
       end
-      
-      new_credentials ||= Credentials.new(nil,nil) 
-      
-      return new_credentials
+
+      raise "No credentials found" if new_credentials.nil?
+
+      new_credentials
     end
   end
 end
